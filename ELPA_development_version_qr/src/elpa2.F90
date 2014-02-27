be241bd84d3125b40b8122c7a0713c93ddf0eda2
Andreas Marek <amarek@rzg.mpg.de>
2014-02-27 15:31:38 +0100

    ELPA_development_version_qr: bugfix for kernels real blocking 6 and 4
    Due to an error in a preprocessor statement, the results for
    real matrices were wrong if the kernels "avx-real-block6" or
    "avx-real-block4" were chosen. No other kernels are affected.
    
    The test programms always correctly stated that the results for
    these kernels are wrong.

--- ELPA_development_version_qr/src/elpa2.F90
+++ ELPA_development_version_qr/src/elpa2.F90
@@ -2559,7 +2559,7 @@ subroutine trans_ev_tridi_to_band_real(na, nev, nblk, nbw, q, ldq, mpi_comm_rows
 #endif
 #endif
 
-#if (defined(WITH_AVX_REAL_BLOCK4) && defined(WITH_AVX_REAL_BLOCK2)) || defined(WITH_AMD_BULLDOZER)
+#if defined(WITH_AVX_REAL_BLOCK4)  || defined(WITH_AMD_BULLDOZER)
 
          ! X86 INTRINSIC CODE, USING 4 HOUSEHOLDER VECTORS
          do j = ncols, 4, -4
@@ -2598,7 +2598,7 @@ subroutine trans_ev_tridi_to_band_real(na, nev, nblk, nbw, q, ldq, mpi_comm_rows
 #endif
 
 
-#if (defined(WITH_AVX_REAL_BLOCK6) && defined(WITH_AVX_REAL_BLOCK4) && defined(WITH_AVX_REAL_BLOCK2))
+#if defined(WITH_AVX_REAL_BLOCK6) 
          ! X86 INTRINSIC CODE, USING 6 HOUSEHOLDER VECTORS
          do j = ncols, 6, -6
              w(:,1) = bcast_buffer(1:nbw,j+off)
