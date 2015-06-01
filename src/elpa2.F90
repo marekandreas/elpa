f485da2e8ed4b56e773a6d0a08ac6960bed19837
Andreas Marek <amarek@rzg.mpg.de>
2015-06-01 08:58:08 +0200

    Port back of memory leak fix

--- src/elpa2.F90
+++ src/elpa2.F90
@@ -1739,7 +1739,7 @@ subroutine tridiag_band_real(na, nb, nblk, a, lda, d, e, mpi_comm_rows, mpi_comm
 #ifdef WITH_OPENMP
              call mpi_wait(ireq_hv,MPI_STATUS_IGNORE,mpierr)
 #else
-             call mpi_wait(ireq_ab,MPI_STATUS_IGNORE,mpierr)
+             call mpi_wait(ireq_hv,MPI_STATUS_IGNORE,mpierr)
 #endif
              hv_s(1) = tau_new
              hv_s(2:) = hv_new(2:)
