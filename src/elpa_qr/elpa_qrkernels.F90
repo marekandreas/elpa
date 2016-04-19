f568656c350a502e605446ce85a2217c51c10351
Andreas Marek <amarek@rzg.mpg.de>
2016-04-19 08:35:53 +0200

    Forgotten file elpa_qrkernels.F90

---
+++ src/elpa_qr/elpa_qrkernels.F90
@@ -0,0 +1,61 @@
+!    This file is part of ELPA.
+!
+!    The ELPA library was originally created by the ELPA consortium,
+!    consisting of the following organizations:
+!
+!    - Max Planck Computing and Data Facility (MPCDF), formerly known as
+!      Rechenzentrum Garching der Max-Planck-Gesellschaft (RZG),
+!    - Bergische Universität Wuppertal, Lehrstuhl für angewandte
+!      Informatik,
+!    - Technische Universität München, Lehrstuhl für Informatik mit
+!      Schwerpunkt Wissenschaftliches Rechnen ,
+!    - Fritz-Haber-Institut, Berlin, Abt. Theorie,
+!    - Max-Plack-Institut für Mathematik in den Naturwissenschaftrn,
+!      Leipzig, Abt. Komplexe Strukutren in Biologie und Kognition,
+!      and
+!    - IBM Deutschland GmbH
+!
+!
+!    More information can be found here:
+!    http://elpa.mpcdf.mpg.de/
+!
+!    ELPA is free software: you can redistribute it and/or modify
+!    it under the terms of the version 3 of the license of the
+!    GNU Lesser General Public License as published by the Free
+!    Software Foundation.
+!
+!    ELPA is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public License
+!    along with ELPA.  If not, see <http://www.gnu.org/licenses/>
+!
+!    ELPA reflects a substantial effort on the part of the original
+!    ELPA consortium, and we ask you to respect the spirit of the
+!    license that we chose: i.e., please contribute any changes you
+!    may have back to the original ELPA library distribution, and keep
+!    any derivatives of ELPA under the same license that we chose for
+!    the original distribution, the GNU Lesser General Public License.
+!
+
+#include "config-f90.h"
+
+
+#define DOUBLE_PRECISION_REAL 1
+#define REAL_DATATYPE rk8
+#include "elpa_qrkernels.X90"
+#undef DOUBLE_PRECISION_REAL
+#undef REAL_DATATYPE
+
+#ifdef WANT_SINGLE_PRECISION_REAL
+
+#undef DOUBLE_PRECISION_REAL
+#define REAL_DATATYPE rk4
+#include "elpa_qrkernels.X90"
+#undef DOUBLE_PRECISION_REAL
+#undef REAL_DATATYPE
+
+#endif /* WANT_SINGLE_PRECISION_REAL */
+
