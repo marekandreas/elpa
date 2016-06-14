5c609a1bd56e559a21151c91d2722bf1594bbe3f
Andreas Marek <amarek@rzg.mpg.de>
2016-06-14 15:15:08 +0200

    Make solve_tridi also public, since needed by FHI-aims
    The subroutine solve_tridi has NOT been shifted from the private
    module "elpa1_compute" to the public module elpa1_auxilliary.
    
    Instead, a wrapper function solve_tride has been introduced, which
    calls (via Fortran module association) the private function.
    
    This does what it is supposed to do, but should be cleaned up at
    some time. Public functions (interfaces) should be implemented in
    a public module

--- src/elpa1.F90
+++ src/elpa1.F90
@@ -107,7 +107,7 @@ module ELPA1
 
 
 !> \brief get_elpa_row_col_comms:  old, deprecated Fortran function to create the MPI communicators for ELPA. Better use "elpa_get_communicators"
-!> \detail
+!> \details
 !> The interface and variable definition is the same as in "elpa_get_communicators"
 !> \param  mpi_comm_global   Global communicator for the calculations (in)
 !>
@@ -125,7 +125,7 @@ module ELPA1
 
 !> \brief solve_evp_real: old, deprecated Fortran function to solve the real eigenvalue problem with 1-stage solver. Better use "solve_evp_real_1stage"
 !>
-!> \detail
+!> \details
 !>  The interface and variable definition is the same as in "elpa_solve_evp_real_1stage"
 !  Parameters
 !
@@ -166,7 +166,7 @@ module ELPA1
 
 !> \brief solve_evp_complex: old, deprecated Fortran function to solve the complex eigenvalue problem with 1-stage solver. Better use "solve_evp_complex_1stage"
 !>
-!> \detail
+!> \details
 !> The interface and variable definition is the same as in "elpa_solve_evp_complex_1stage"
 !  Parameters
 !
