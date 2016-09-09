1c9022613c96071fba51e5cb829c05706048ea29
Andreas Marek <amarek@rzg.mpg.de>
2016-09-09 12:32:45 +0200

    Update headers of complex test program

--- test/Fortran/test_complex2_api_single.F90
+++ test/Fortran/test_complex2_api_single.F90
@@ -238,6 +238,13 @@ program test_complex2_choose_kernel_with_api_single_precision
 #ifdef WITH_COMPLEX_SSE_ASSEMBLY_KERNEL
       print *, " The settings are: COMPLEX_ELPA_KERNEL_SSE"
 #endif
+#ifdef WITH_COMPLEX_SSE_BLOCK1_KERNEL
+      print *, " The settings are: COMPLEX_ELPA_KERNEL_SSE_BLOCK1"
+#endif
+
+#ifdef WITH_COMPLEX_SSE_BLOCK2_KERNEL
+      print *, " The settings are: COMPLEX_ELPA_KERNEL_SSE_BLOCK2"
+#endif
 
 #ifdef WITH_COMPLEX_AVX_BLOCK1_KERNEL
       print *, " The settings are: COMPLEX_ELPA_KERNEL_AVX_BLOCK1"
@@ -245,6 +252,22 @@ program test_complex2_choose_kernel_with_api_single_precision
 
 #ifdef WITH_COMPLEX_AVX_BLOCK2_KERNEL
       print *, " The settings are: COMPLEX_ELPA_KERNEL_AVX_BLOCK2"
+#endif
+
+#ifdef WITH_COMPLEX_AVX2_BLOCK1_KERNEL
+      print *, " The settings are: COMPLEX_ELPA_KERNEL_AVX2_BLOCK1"
+#endif
+
+#ifdef WITH_COMPLEX_AVX2_BLOCK2_KERNEL
+      print *, " The settings are: COMPLEX_ELPA_KERNEL_AVX2_BLOCK2"
+#endif
+
+#ifdef WITH_COMPLEX_AVX512_BLOCK1_KERNEL
+      print *, " The settings are: COMPLEX_ELPA_KERNEL_AVX512_BLOCK1"
+#endif
+
+#ifdef WITH_COMPLEX_AVX512_BLOCK2_KERNEL
+      print *, " The settings are: COMPLEX_ELPA_KERNEL_AVX512_BLOCK2"
 #endif
 
 #ifdef WITH_GPU_VERSION
