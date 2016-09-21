2cf75bf3cef1362d5ec7843b0cce58ebfd863e9c
Andreas Marek <amarek@rzg.mpg.de>
2016-09-21 11:06:16 +0200

    Remove uneccessary limiting of qr decomposition test

--- test/Fortran/test_real2_qr_single.F90
+++ test/Fortran/test_real2_qr_single.F90
@@ -151,27 +151,27 @@ program test_real2_default_kernel_qr_decomposition_single_precision
    !  stop 1
    !endif
 
-   ! override nblk
-      nblk = 2
-      na   = 4000
-      nev  = 1500
-!       nblk = 32
-   !   na   = 4000
-   !   nev  = 1500
-
-   ! make sure na, nbl is even
-   if (mod(nblk,2 ) .ne. 0) then
-     nblk = nblk - 1
-   endif
-
-   ! make sure na is even
-   if (mod(na,2) .ne. 0) then
-     na = na - 1
-   endif
-   ! make sure na is at least 34
-   if (na .lt. 34) then
-     na = 34
-   endif
+!   ! override nblk
+!      nblk = 2
+!      na   = 4000
+!      nev  = 1500
+!!       nblk = 32
+!   !   na   = 4000
+!   !   nev  = 1500
+!
+!   ! make sure na, nbl is even
+!   if (mod(nblk,2 ) .ne. 0) then
+!     nblk = nblk - 1
+!   endif
+!
+!   ! make sure na is even
+!   if (mod(na,2) .ne. 0) then
+!     na = na - 1
+!   endif
+!   ! make sure na is at least 34
+!   if (na .lt. 34) then
+!     na = 34
+!   endif
 
    !-------------------------------------------------------------------------------
    !  MPI Initialization
@@ -180,6 +180,20 @@ program test_real2_default_kernel_qr_decomposition_single_precision
    gpuAvailable = check_for_gpu(myid, numberOfDevices)
 
    STATUS = 0
+
+   if (nblk .lt. 64) then
+     status = 1
+     if (myid .eq. 0) print *,"At the moment QR decomposition need blocksize of at least 64"
+     if (na .lt. 64) then
+       if (myid .eq. 0) print *,"This is why the matrix size must also be at least 64 or only 1 MPI task can be used"
+     endif
+
+#ifdef WITH_MPI
+      call mpi_finalize(mpierr)
+#endif
+      call EXIT(0)
+   endif
+
 #define REALCASE
 #include "elpa_print_headers.X90"
 
