7affd757bb6c064c69ef84dff43868ed0d02c3d4
Andreas Marek <amarek@rzg.mpg.de>
2016-10-12 08:36:03 +0200

    Better print statements in complex ELPA2 c-version

--- test/C/elpa2_test_complex_c_version.c
+++ test/C/elpa2_test_complex_c_version.c
@@ -109,13 +109,13 @@ int main(int argc, char** argv) {
    if (myid == 0) {
      printf("This is the c version of an ELPA test-programm\n");
      printf("\n");
-     printf("It will call the 1stage ELPA complex solver for a matrix\n");
+     printf("It will call the 2stage ELPA complex solver for a matrix\n");
      printf("of matrix size %d. It will compute %d eigenvalues\n",na,nev);
      printf("and uses a blocksize of %d\n",nblk);
      printf("\n");
      printf("This is an example program with much less functionality\n");
      printf("as it's Fortran counterpart. It's only purpose is to show how \n");
-     printf("to evoke ELPA1 from a c programm\n");
+     printf("to evoke ELPA 2 from a c programm\n");
 
      printf("\n");
 
