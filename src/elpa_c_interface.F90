c7b5d9da488ea37c60cd65ff402820a15920be18
Andreas Marek <amarek@rzg.mpg.de>
2017-04-05 14:51:13 +0200

    Template for elpa_cholesky C-interface

--- src/elpa_c_interface.F90
+++ src/elpa_c_interface.F90
@@ -994,38 +994,12 @@
  !c> */
 
  !c> int elpa_cholesky_real_double(int na, double *a, int lda, int nblk, int matrixCols, int mpi_comm_rows, int mpi_comm_cols, int wantDebug);
- function elpa_cholesky_real_wrapper_double(na, a, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebug) result(success) &
-       bind(C,name="elpa_cholesky_real_double")
-
-   use, intrinsic :: iso_c_binding
-   use elpa1_auxiliary, only : elpa_cholesky_real_double
-
-   implicit none
-
-   integer(kind=c_int), value :: na, lda, nblk, matrixCols,  mpi_comm_rows, mpi_comm_cols, wantDebug
-   integer(kind=c_int)        :: success
-#ifdef USE_ASSUMED_SIZE
-   real(kind=c_double)        :: a(lda,*)
-#else
-   real(kind=c_double)        :: a(lda,matrixCols)
-#endif
-   logical                    :: successFortran, wantDebugFortran
-
-   if (wantDebug .ne. 0) then
-     wantDebugFortran = .true.
-   else
-     wantDebugFortran = .false.
-   endif
-
-   successFortran = elpa_cholesky_real_double(na, a, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebugFortran)
-
-   if (successFortran) then
-     success = 1
-   else
-     success = 0
-   endif
-
- end function
+#define REALCASE 1
+#define DOUBLE_PRECISION 1
+#include "precision_macros.h"
+#include "elpa_cholesky_c_interface_template.X90"
+#undef DOUBLE_PRECISION
+#undef REALCASE
 
 #ifdef WANT_SINGLE_PRECISION_REAL
 
@@ -1049,35 +1023,13 @@
  !c> */
 
  !c> int elpa_cholesky_real_single(int na, float *a, int lda, int nblk, int matrixCols, int mpi_comm_rows, int mpi_comm_cols, int wantDebug);
- function elpa_cholesky_real_wrapper_single(na, a, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebug) result(success) &
-       bind(C,name="elpa_cholesky_real_single")
+#define REALCASE 1
+#define SINGLE_PRECISION 1
+#include "precision_macros.h"
+#include "elpa_cholesky_c_interface_template.X90"
+#undef SINGLE_PRECISION
+#undef REALCASE
 
-   use, intrinsic :: iso_c_binding
-   use elpa1_auxiliary, only : elpa_cholesky_real_single
-
-   implicit none
-
-   integer(kind=c_int), value :: na, lda, nblk, matrixCols,  mpi_comm_rows, mpi_comm_cols, wantDebug
-   integer(kind=c_int)        :: success
-   real(kind=c_float)         :: a(lda,matrixCols)
-
-   logical                    :: successFortran, wantDebugFortran
-
-   if (wantDebug .ne. 0) then
-     wantDebugFortran = .true.
-   else
-     wantDebugFortran = .false.
-   endif
-
-   successFortran = elpa_cholesky_real_single(na, a, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebugFortran)
-
-   if (successFortran) then
-     success = 1
-   else
-     success = 0
-   endif
-
- end function
 
 #endif /* WANT_SINGLE_PRECISION_REAL */
 
@@ -1100,40 +1052,12 @@
  !c> */
 
  !c> int elpa_cholesky_complex_double(int na, double complex *a, int lda, int nblk, int matrixCols, int mpi_comm_rows, int mpi_comm_cols, int wantDebug);
- function elpa_cholesky_complex_wrapper_double(na, a, lda, nblk, matrixCols, mpi_comm_rows, &
-                                               mpi_comm_cols, wantDebug) result(success) &
-       bind(C,name="elpa_cholesky_complex_double")
-
-   use, intrinsic :: iso_c_binding
-   use elpa1_auxiliary, only : elpa_cholesky_complex_double
-
-   implicit none
-
-   integer(kind=c_int), value     :: na, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebug
-   integer(kind=c_int)            :: success
-#ifdef USE_ASSUMED_SIZE
-   complex(kind=c_double_complex) :: a(lda,*)
-#else
-   complex(kind=c_double_complex) :: a(lda,matrixCols)
-#endif
-   logical                        :: wantDebugFortran, successFortran
-
-   if (wantDebug .ne. 0) then
-     wantDebugFortran = .true.
-   else
-     wantDebugFortran = .false.
-   endif
-
-   successFortran = elpa_cholesky_complex_double(na, a, lda, nblk, matrixCols, &
-                                                 mpi_comm_rows, mpi_comm_cols, wantDebugFortran)
-
-   if (successFortran) then
-     success = 1
-   else
-     success = 0
-   endif
-
- end function
+#define COMPLEXCASE 1
+#define DOUBLE_PRECISION 1
+#include "precision_macros.h"
+#include "elpa_cholesky_c_interface_template.X90"
+#undef DOUBLE_PRECISION
+#undef COMPLEXCASE
 
 #ifdef WANT_SINGLE_PRECISION_COMPLEX
 
@@ -1156,36 +1080,10 @@
  !c> */
 
  !c> int elpa_cholesky_complex_single(int na, complex *a, int lda, int nblk, int matrixCols, int mpi_comm_rows, int mpi_comm_cols, int wantDebug);
- function elpa_cholesky_complex_wrapper_single(na, a, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, &
-                                               wantDebug) result(success) &
-       bind(C,name="elpa_cholesky_complex_single")
-
-   use, intrinsic :: iso_c_binding
-   use elpa1_auxiliary, only : elpa_cholesky_complex_single
-
-   implicit none
-
-   integer(kind=c_int), value     :: na, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebug
-   integer(kind=c_int)            :: success
-
-   complex(kind=c_float_complex)  :: a(lda,matrixCols)
-
-   logical                        :: wantDebugFortran, successFortran
-
-   if (wantDebug .ne. 0) then
-     wantDebugFortran = .true.
-   else
-     wantDebugFortran = .false.
-   endif
-
-   successFortran = elpa_cholesky_complex_single(na, a, lda, nblk, matrixCols, mpi_comm_rows, mpi_comm_cols, wantDebugFortran)
-
-   if (successFortran) then
-     success = 1
-   else
-     success = 0
-   endif
-
- end function
-
+#define COMPLEXCASE 1
+#define SINGLE_PRECISION 1
+#include "precision_macros.h"
+#include "elpa_cholesky_c_interface_template.X90"
+#undef SINGLE_PRECISION
+#undef COMPLEXCASE
 #endif /* WANT_SINGLE_PRECISION_COMPLEX */
