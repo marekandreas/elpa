1f343334a1cfa67d07e833d2c82a955209450a77
Andreas Marek <amarek@rzg.mpg.de>
2017-04-07 14:29:05 +0200

    Put module assert in independent file

--- test/Fortran/test_new_interface_complex.F90
+++ test/Fortran/test_new_interface_complex.F90
@@ -46,22 +46,6 @@
 #define stringify(x) stringify_(x)
 #define assert(x) call x_assert(x, stringify(x), __FILE__, __LINE__)
 
-module assert
-  implicit none
-  contains
-    subroutine x_assert(condition, condition_string, file, line)
-      use elpa_utilities, only : error_unit
-      logical, intent(in) :: condition
-      character(len=*), intent(in) :: condition_string
-      character(len=*), intent(in) :: file
-      integer, intent(in) :: line
-
-      if (.not. condition) then
-        write(error_unit,'(a,i0)') "Assertion failed:" // condition_string // " at " // file // ":", line
-      end if
-    end subroutine
-end module
-
 program test_interface
    use precision
    use assert
