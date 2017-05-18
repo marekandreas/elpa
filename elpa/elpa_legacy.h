bd0fa35b535cec4f6ae9cd7fa630004ab373b914
Andreas Marek <amarek@rzg.mpg.de>
2017-05-18 08:14:10 +0200

    Fix forgotten file

---
+++ elpa/elpa_legacy.h
@@ -0,0 +1,11 @@
+#ifndef ELPA_H
+#define ELPA_H
+
+#include <limits.h>
+
+#include <elpa/elpa_constants.h>
+#include <elpa/elpa_generated_legacy.h>
+
+const char *elpa_strerr(int elpa_error);
+
+#endif
