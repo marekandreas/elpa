4567af7a2b4518cbb2d52e5b9216c317056ae2a5
Pavel Kus <pavel.kus@mpcdf.mpg.de>
2017-06-13 09:48:23 +0200

    assert macro not usable for PGI
    In the current form, the assert macro is not usable, since it might
    generate too long lines depending on the lenght of __FILE__ string.
    This is a temporary fix to allow setting up tests on Minsky

--- test/Fortran/assert.h
+++ test/Fortran/assert.h
@@ -40,7 +40,7 @@
 
 #define stringify_(x) "x"
 #define stringify(x) stringify_(x)
-#define assert(x) call x_a(x, stringify(x), __FILE__, __LINE__)
+#define assert(x) call x_a(x, stringify(x), "F", __LINE__)
 
 #define assert_elpa_ok(error_code) call x_ao(error_code, stringify(error_code), __FILE__, __LINE__)
 
