f6c67a50ecb9e02269a069f746d26d171a1effc3
Andreas Marek <amarek@rzg.mpg.de>
2017-11-28 09:08:57 +0100

    A bit cleanup of deprecated features

--- test_project_1stage_legacy_api/src/test_real.F90
+++ test_project_1stage_legacy_api/src/test_real.F90
@@ -156,9 +156,9 @@ program test_real_example
    na_cols = numroc(na, nblk, my_pcol, 0, np_cols)
 
    ! All ELPA routines need MPI communicators for communicating within
-   ! rows or columns of processes, these are set in get_elpa_communicators.
+   ! rows or columns of processes, these are set in elpa_get_communicators.
 
-   mpierr = get_elpa_communicators(mpi_comm_world, my_prow, my_pcol, &
+   mpierr = elpa_get_communicators(mpi_comm_world, my_prow, my_pcol, &
                                    mpi_comm_rows, mpi_comm_cols)
 
    ! set up the scalapack descriptor for the checks below
