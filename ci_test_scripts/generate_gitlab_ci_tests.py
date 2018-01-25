cc9d59801e3ccd908e0a55b38d8970e7ec8c18dd
Andreas Marek <amarek@rzg.mpg.de>
2018-01-25 15:19:18 +0100

    Re-activate all architecturs in CI

--- ci_test_scripts/generate_gitlab_ci_tests.py
+++ ci_test_scripts/generate_gitlab_ci_tests.py
@@ -425,18 +425,14 @@ coverage = {
         "no-coverage": "no-coverage",
 }
 
-#instruction_set = {
-#        "sse" : " --enable-sse --enable-sse-assembly",
-#        "avx" : " --enable-avx",
-#        "avx2" : " --enable-avx2",
-#        "avx512" : "--enable-avx512",
-#        "knl" : "--enable-avx512",
-#        "power8" : " --enable-vsx --disable-sse --disable-sse-assembly --disable-avx --disable-avx2 --disable-mpi-module --with-GPU-compute-capability=sm_60 ",
-#}
 instruction_set = {
+        "sse" : " --enable-sse --enable-sse-assembly",
+        "avx" : " --enable-avx",
         "avx2" : " --enable-avx2",
+        "avx512" : "--enable-avx512",
+        "knl" : "--enable-avx512",
+        "power8" : " --enable-vsx --disable-sse --disable-sse-assembly --disable-avx --disable-avx2 --disable-mpi-module --with-GPU-compute-capability=sm_60 ",
 }
-
 
 address_sanitize_flag = {
         "address-sanitize" : "address-sanitize",
