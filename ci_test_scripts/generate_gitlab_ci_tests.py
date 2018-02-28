f92468a00e2518df8bc92b6851450ee84f60518a
Andreas Marek <amarek@rzg.mpg.de>
2018-02-28 07:46:51 +0100

    Gitlab CI: Deactivate KNL tests

--- ci_test_scripts/generate_gitlab_ci_tests.py
+++ ci_test_scripts/generate_gitlab_ci_tests.py
@@ -430,12 +430,12 @@ coverage = {
         "no-coverage": "no-coverage",
 }
 
+        #"knl" : "--enable-avx512",
 instruction_set = {
         "sse" : " --enable-sse --enable-sse-assembly",
         "avx" : " --enable-avx",
         "avx2" : " --enable-avx2",
         "avx512" : "--enable-avx512",
-        "knl" : "--enable-avx512",
         "power8" : " --enable-vsx --disable-sse --disable-sse-assembly --disable-avx --disable-avx2 --disable-mpi-module --with-GPU-compute-capability=sm_60 ",
 }
 
