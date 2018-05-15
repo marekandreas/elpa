2165b222f86094c3434cd0cf2fa82716099d01f2
Lorenz Hüdepohl <lorenz.huedepohl@rzg.mpg.de>
2018-05-15 16:29:40 +0200

    Undo copy&paste of generate_automake_test_programs.py

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,5 @@ mkdir -p m4/
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
-$srcdir/generate_automake_Fortran_test_programs.py > $srcdir/Fortran_test_programs.am
-$srcdir/generate_automake_C_test_programs.py > $srcdir/C_test_programs.am
+$srcdir/generate_automake_test_programs.py > $srcdir/test_programs.am
 autoreconf --force --install --verbose "$srcdir"
