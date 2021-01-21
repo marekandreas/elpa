b79fac39fa5bb3ca74dff9effcafbd524f1cce14
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2021-01-21 14:06:31 +0100

    Gitlab CI: update file to create YAML

--- ci_test_scripts/generate_gitlab_ci_tests.py
+++ ci_test_scripts/generate_gitlab_ci_tests.py
@@ -610,10 +610,14 @@ coverage = {
         #"knl" : "--enable-avx512",
         #"power8" : " --enable-vsx --disable-sse --disable-sse-assembly --disable-avx --disable-avx2 --disable-avx512 --disable-mpi-module --with-GPU-compute-capability=sm_60 ",
 
+#instruction_set = {
+#        "sse" : " --enable-sse --enable-sse-assembly",
+#        "avx" : " --enable-avx",
+#        "avx2" : " --enable-avx2",
+#        "avx512" : "--enable-avx512",
+#}
+
 instruction_set = {
-        "sse" : " --enable-sse --enable-sse-assembly",
-        "avx" : " --enable-avx",
-        "avx2" : " --enable-avx2",
         "avx512" : "--enable-avx512",
 }
 
