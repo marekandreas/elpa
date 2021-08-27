4251b0d50ca5998e1824d82ca199b171ff512ebb
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2021-08-27 14:40:09 +0200

    Test with all CI tests

--- ci_test_scripts/generate_gitlab_ci_tests.py
+++ ci_test_scripts/generate_gitlab_ci_tests.py
@@ -764,20 +764,20 @@ for cc, fc, m, o, p, a, b, g, instr, addr, na in product(
         MasterOnly=True
     if (cov == "coverage"):
         MasterOnly=True
-    if (p == "double-precision"):
-        MasterOnly=True
+    #if (p == "double-precision"):
+    #    MasterOnly=True
     if (instr != "avx" and instr != "sse" and addr == "address-sanitize"):
         MasterOnly=True
 
     # make non-master tests even faster
     # kicking out gpu is not good, but at the momemt we have a real problem with gpu runners
     # should be returned when solved
-    if (g == "with-gpu"):
-        MasterOnly=True
-    if (a == "no-assumed-size"):
-        MasterOnly=True
-    if (instr == "avx2" or instr == "avx512"):
-        MasterOnly=True
+    #if (g == "with-gpu"):
+    #    MasterOnly=True
+    #if (a == "no-assumed-size"):
+    #    MasterOnly=True
+    #if (instr == "avx2" or instr == "avx512"):
+    #    MasterOnly=True
 
     print("# " + cc + "-" + fc + "-" + m + "-" + o + "-" + p + "-" + a + "-" + b + "-" +g + "-" + cov + "-" + instr + "-" + addr)
     print(cc + "-" + fc + "-" + m + "-" + o + "-" + p + "-" +a + "-" +b + "-" +g + "-" + cov + "-" + instr + "-" + addr + "-jobs:")
