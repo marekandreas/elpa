2c8ad0330e89b10c89974037829749be6382f81e
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2022-03-24 14:56:00 +0100

    Adapt elpa_index for SYCL

--- src/elpa_index.c
+++ src/elpa_index.c
@@ -121,7 +121,7 @@ int nvidia_gpu_count();
 #ifdef WITH_AMD_GPU_VERSION
 int amd_gpu_count();
 #endif
-#ifdef WITH_INTEL_GPU_VERSION
+#if defined(WITH_INTEL_GPU_VERSION) || defined(WITH_OPENMP_OFFLOAD_GPU_VERSION) || defined(WITH_SYCL_GPU_VERSION)
 //missing function for GPU count
 #endif
 #ifdef WITH_OPENMP_OFFLOAD_GPU_VERSION
@@ -885,7 +885,10 @@ static int real_kernel_is_valid(elpa_index_t index, int n, int new_value) {
 #ifdef WITH_OPENMP_OFFLOAD_GPU_VERSION
                 ELPA_FOR_ALL_2STAGE_REAL_KERNELS(VALID_CASE_3, REAL_INTEL_GPU_KERNEL_ONLY_WHEN_GPU_IS_ACTIVE)
 #endif
-#if !defined(WITH_NVIDIA_GPU_VERSION) && !defined(WITH_AMD_GPU_VERSION) && !defined(WITH_INTEL_GPU_VERSION) && !defined(WITH_OPENMP_OFFLOAD_GPU_VERSION)
+#ifdef WITH_SYCL_GPU_VERSION
+                ELPA_FOR_ALL_2STAGE_REAL_KERNELS(VALID_CASE_3, REAL_INTEL_GPU_KERNEL_ONLY_WHEN_GPU_IS_ACTIVE)
+#endif
+#if !defined(WITH_NVIDIA_GPU_VERSION) && !defined(WITH_AMD_GPU_VERSION) && !defined(WITH_INTEL_GPU_VERSION) && !defined(WITH_OPENMP_OFFLOAD_GPU_VERSION) && !defined(WITH_SYCL_GPU_VERSION)
                 ELPA_FOR_ALL_2STAGE_REAL_KERNELS(VALID_CASE_3, REAL_NVIDIA_GPU_KERNEL_ONLY_WHEN_GPU_IS_ACTIVE)
 #endif
 		// intel missing
@@ -945,7 +948,10 @@ static int complex_kernel_is_valid(elpa_index_t index, int n, int new_value) {
 #ifdef WITH_OPENMP_OFFLOAD_GPU_VERSION
                 ELPA_FOR_ALL_2STAGE_COMPLEX_KERNELS(VALID_CASE_3, COMPLEX_INTEL_GPU_KERNEL_ONLY_WHEN_GPU_IS_ACTIVE)
 #endif
-#if !defined(WITH_NVIDIA_GPU_VERSION) && !defined(WITH_AMD_GPU_VERSION) && !defined(WITH_INTEL_GPU_VERSION) && !defined(WITH_OPENMP_OFFLOAD_GPU_VERSION)
+#ifdef WITH_SYCL_GPU_VERSION
+                ELPA_FOR_ALL_2STAGE_COMPLEX_KERNELS(VALID_CASE_3, COMPLEX_INTEL_GPU_KERNEL_ONLY_WHEN_GPU_IS_ACTIVE)
+#endif
+#if !defined(WITH_NVIDIA_GPU_VERSION) && !defined(WITH_AMD_GPU_VERSION) && !defined(WITH_INTEL_GPU_VERSION) && !defined(WITH_OPENMP_OFFLOAD_GPU_VERSION) && !defined(WITH_SYCL_GPU_VERSION)
                 ELPA_FOR_ALL_2STAGE_COMPLEX_KERNELS(VALID_CASE_3, COMPLEX_NVIDIA_GPU_KERNEL_ONLY_WHEN_GPU_IS_ACTIVE)
 #endif
 		// intel missing
@@ -1025,7 +1031,7 @@ static int amd_gpu_is_valid(elpa_index_t index, int n, int new_value) {
 }
 
 static int intel_gpu_is_valid(elpa_index_t index, int n, int new_value) {
-#if defined(WITH_INTEL_GPU_VERSION) || defined(WITH_OPENMP_OFFLOAD_GPU_VERSION)
+#if defined(WITH_INTEL_GPU_VERSION) || defined(WITH_OPENMP_OFFLOAD_GPU_VERSION) || defined(WITH_SYCL_GPU_VERSION)
         return new_value == 0 || new_value == 1;
 #else
         return new_value == 0;
@@ -1311,6 +1317,8 @@ static int use_gpu_id_cardinality(elpa_index_t index) {
 	return 0;
 #elif WITH_OPENMP_OFFLOAD_GPU_VERSION
 	return 0;
+#elif WITH_SYCL_GPU_VERSION
+	return 0;
 #else
 	return 0;
 #endif
@@ -1344,6 +1352,8 @@ static int use_gpu_id_is_valid(elpa_index_t index, int n, int new_value) {
 #elif WITH_INTEL_GPU_VERSION
 	return 0 == 0;
 #elif WITH_OPENMP_OFFLOAD_GPU_VERSION
+	return 0 == 0;
+#elif WITH_SYCL_GPU_VERSION
 	return 0 == 0;
 #else
 	return 0 == 0;
