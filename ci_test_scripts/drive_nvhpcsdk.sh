31a9030a01c4c738a20fa2786068781686616aca
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2022-04-05 18:11:08 +0200

    CI: tests for nvhpcsdk

---
+++ ci_test_scripts/drive_nvhpcsdk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -x
+
+#some defaults
+mpiTasks=$1
+cp /u/elpa/runners/job_script_templates/run_raven_1node_4GPU_layouts.sh .
+
+perl -i -p -e "s/TASKS/$mpiTasks/g" ./run_raven_1node_4GPU_layouts.sh
+
+sbatch -W run_raven_1node_4GPU_layouts.sh
+exitCode=$?
+if [ $exitCode -ne 0 ]; then exit 1; fi
+exit 0;
+
