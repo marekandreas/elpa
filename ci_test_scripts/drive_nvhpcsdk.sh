9b6c63da25c8fcad2501edad462a26e0b24165a0
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2022-04-14 08:36:44 +0200

    CI: correct driver for nvhpcsdk

--- ci_test_scripts/drive_nvhpcsdk.sh
+++ ci_test_scripts/drive_nvhpcsdk.sh
@@ -4,11 +4,11 @@ set -x
 
 #some defaults
 mpiTasks=$1
-cp /u/elpa/runners/job_script_templates/run_raven_1node_4GPU_layouts.sh .
+cp /u/elpa/runners/job_script_templates/run_raven_1node_4GPU_nvhpcsdk.sh .
 
-perl -i -p -e "s/TASKS/$mpiTasks/g" ./run_raven_1node_4GPU_layouts.sh
+perl -i -p -e "s/TASKS/$mpiTasks/g" ./run_raven_1node_4GPU_nvhpcsdk.sh
 
-sbatch -W run_raven_1node_4GPU_layouts.sh
+sbatch -W run_raven_1node_4GPU_nvhpcsdk.sh
 exitCode=$?
 if [ $exitCode -ne 0 ]; then exit 1; fi
 exit 0;
