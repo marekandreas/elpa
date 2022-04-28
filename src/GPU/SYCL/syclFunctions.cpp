703135d4d1be669ec09d77eba011e1d018c14d83
Andreas Marek <andreas.marek@mpcdf.mpg.de>
2022-04-28 17:47:09 +0200

    Fix checking Intel GPUs in elpa_index.c

--- src/GPU/SYCL/syclFunctions.cpp
+++ src/GPU/SYCL/syclFunctions.cpp
@@ -60,34 +60,9 @@
 #include "config-f90.h"
 
 #ifdef WITH_SYCL_GPU_VERSION
-using namespace cl;
-
-static std::vector<sycl::device> devices;
-static std::optional<sycl::queue> chosenDeviceQueue{};
-
-bool deviceCollectionFlag = false;
+#include "./collectGpuDevices.h"
 
 extern "C" {
-
-static void collectGpuDevices() {
-  if (deviceCollectionFlag) {
-    return;
-  }
-
-  for (auto const &p : sycl::platform::get_platforms()) {
-    for (auto dev : p.get_devices()) {
-      using namespace sycl::info;
-// why this?
-#ifndef ENABLE_ALL_DEVICES
-      if (dev.get_info<device::device_type>() == device_type::gpu)
-#endif
-      {
-        devices.push_back(dev);
-      }
-
-    }
-  }
-}
 
 static oneapi::mkl::transpose transposeFromChar(char c) {
   switch (c) {
